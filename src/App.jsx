977ec0467d7e20ea9a37f8b97670725333e3c449
Zeusmoz <75400470+Zeusmoz@users.noreply.github.com>
2026-02-19 17:32:03 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Pipeline from './pages/Pipeline'
+import Login from './pages/Login'
+import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
-import { Routes, Route } from 'react-router-dom'
-  import Home from './pages/Home'
-    import Pipeline from './pages/Pipeline'
-      import Login from './pages/Login'
-        import ProtectedRoute from './components/ProtectedRoute'
-          
-  export default function App() {
-      return (
-            <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/pipeline" element={
-                            <ProtectedRoute>
-                                      <Pipeline />
-                            </ProtectedRoute>ProtectedRoute>
-                    } />
-                  </Route>Routes>
-              )
-              }</Routes>return (
+  return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/pipeline" element={<Pipeline />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/pipeline" element={
+        <ProtectedRoute>
+          <Pipeline />
+        </ProtectedRoute>
+      } />
     </Routes>
   )
 }
