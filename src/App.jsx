1fa8132cc1cf2285af57ca2533ffecef27598819
Zeusmoz <75400470+Zeusmoz@users.noreply.github.com>
2026-02-19 15:19:07 -0400

    Add login auth - protect /pipeline route

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,25 @@ import Home from './pages/Home'
 import Pipeline from './pages/Pipeline'
 
 export default function App() {
-  return (
+import { Routes, Route } from 'react-router-dom'
+  import Home from './pages/Home'
+    import Pipeline from './pages/Pipeline'
+      import Login from './pages/Login'
+        import ProtectedRoute from './components/ProtectedRoute'
+          
+  export default function App() {
+      return (
+            <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/pipeline" element={
+                            <ProtectedRoute>
+                                      <Pipeline />
+                            </ProtectedRoute>ProtectedRoute>
+                    } />
+                  </Route>Routes>
+              )
+              }</Routes>return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/pipeline" element={<Pipeline />} />
